29851b94bfa564eb7efbf43d1da6df61878be566
Sakshi khairkar <119487266+Sakshi8830@users.noreply.github.com>
2023-03-31 15:40:45 +0530

    Update Pizza_Bill.java

--- Pizza_Bill.java
+++ Pizza_Bill.java
@@ -3,12 +3,21 @@ package demopizzafactory;
 public class Pizza_Bill {
 	
 	public void getBill(int total) {
-//		Veg_Pizza vp=new Veg_Pizza();
-//		Basic_pizza bp=new Basic_pizza();
+
 		Common_menu cp=new Common_menu();
 		
-		
-		System.out.println("Qty::"+ (total+cp.getExtraCheese()+cp.getSides()));
+        System.out.println("\n********** Your Invoice ***********\n\n\tPay amount::"+ (total+cp.getExtraCheese()+cp.getSides()) +"/-\n\n");
+        
 	}
+	
+	public void getNonVegBill(int nonvegtotal) {
+
+		Common_menu cp=new Common_menu();
+		
+        System.out.println("\n********** Your Invoice ***********\n\n\tPay amount::"+(nonvegtotal+cp.getExtraCheese()+cp.getSides())+"/-\n\n");
+        
+	}
+	
+
 	
 }
